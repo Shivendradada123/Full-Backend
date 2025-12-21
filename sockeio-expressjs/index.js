c4d9631365f97c2e5b519b2373ee481f2fc465e5
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-12-21 22:45:46 +0530

    client added for testin the forntend plus backed

--- sockeio-expressjs/index.js
+++ sockeio-expressjs/index.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const dotenv = require('dotenv')
 const homeRoute = require("./router/home.controller")
+const {Server} = require("socket.io")
+const http = require("http")
 // initialise the dotenv
 dotenv.config()
 
@@ -11,8 +13,26 @@ const app = express()
 app.use(express.json())
 app.use("/",homeRoute)
 
+// create seerver for the socket io
+const server = http.createServer(app)
 
+const io = new Server(server,{
+    cors:{
+        origin:['http://localhost:5173'],
+        methods:["GET","POST"],
+        credentials:true
+    }
+})
 
+io.on("connection",(socket)=>{
+    console.log("user connected: ", socket.id)
+
+    socket.on("test",(data)=>{
+        console.log(data)
+
+        io.emit("test2",data)
+    })
+})
 app.listen(port, ()=>{
     console.log(`server running at http://localhost:${port}`)
 })
