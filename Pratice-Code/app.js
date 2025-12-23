271ded35b914b6ab99029d77eb18ae6d40131927
Shivedra123 <shivendrasaket2580@gmail.com>
2025-12-23 08:13:54 +0530

    pratice code basic

---
+++ Pratice-Code/app.js
@@ -0,0 +1,11 @@
+const http =  require('http');
+
+const server = http.createServer((req,res)=>{
+  console.log(req.url)
+});
+
+const PORT = 3000;
+
+server.listen(PORT, ()=>{
+  console.log(`server is running on http://localhost:${PORT}`)
+});
