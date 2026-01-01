13fb4e07b1f54a2dbe96dfb686085158e330a93d
Shivedra123 <shivendrasaket2580@gmail.com>
2026-01-01 17:00:25 +0530

    routing

--- Express js/server.js
+++ Express js/server.js
@@ -5,13 +5,40 @@ const express = require('express');
 // const handler = require('./app')
 const app = express()
 
-app.use((req,res,next)=>{
+app.use('/', (req,res,next)=>{
   console.log( 'hey this is a first middleware', req.url, req.method)
+  
+
+const express = require('express');
+
+// const handler = require('./app')
+const app = express()
+
+app.use('/', (req,res,next)=>{
+  console.log( 'hey this is a first middleware', req.url, req.method)
+   res.send('<h1> Happy New Year 2025</h1>')
   next()
 });
-app.use((req,res,next)=>{
+app.use('/2026',(req,res,next)=>{
   console.log( 'hey this is a Second middleware', req.url, req.method)
-  res.send('<h1>LING ka Happy New Year</h1>')
+  res.send('<h1> Happy New Year 2026</h1>')
+});
+
+
+
+
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`server is running at http://localhost:${PORT}`);
+});
+
+   
+  next()
+});
+app.use('/2026',(req,res,next)=>{
+  console.log( 'hey this is a Second middleware', req.url, req.method)
+  res.send('<h1> Happy New Year 2026</h1>')
 });
 
 
--- README.md
+++ README.md
@@ -536,3 +536,31 @@ server.listen(PORT, () => {
 });
 ```
 
+## Express DeepDive
+
+```
+
+
+const express = require('express');
+
+// const handler = require('./app')
+const app = express()
+
+app.use((req,res,next)=>{
+  console.log( 'hey this is a first middleware', req.url, req.method)
+  next()
+});
+app.use((req,res,next)=>{
+  console.log( 'hey this is a Second middleware', req.url, req.method)
+  res.send('<h1>LING ka Happy New Year</h1>')
+});
+
+
+
+
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`server is running at http://localhost:${PORT}`);
+});
+```
