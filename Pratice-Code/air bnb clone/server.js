0aefaea5c2f5a615f47d692f4f7409913a2bc3ee
Shivedra123 <shivendrasaket2580@gmail.com>
2026-01-06 21:08:49 +0530

    server code create

--- Pratice-Code/air bnb clone/server.js
+++ Pratice-Code/air bnb clone/server.js
@@ -1,1 +1,18 @@
-const express = re
+const express = require('express');
+
+
+const app = express();
+
+app.use((req,res,next)=>{
+console.log(req.url, req.method )
+res.send('Hello Shivendra')
+
+})
+
+
+
+const PORT = 3000;
+
+app.listen(PORT,()=>{
+  console.log(`server is running on http://localhost:${PORT}`)
+})
