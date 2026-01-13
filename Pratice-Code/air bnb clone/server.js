8cd29a4e6e08a3e3b4ffe9e140167e7abdb47cd4
Shivedra123 <shivendrasaket2580@gmail.com>
2026-01-13 21:14:38 +0530

    server

--- Pratice-Code/air bnb clone/server.js
+++ Pratice-Code/air bnb clone/server.js
@@ -1,38 +1,19 @@
 const express = require('express');
 
+
 const app = express();
 
-app.use(express.json());
 
-let users = [
-  { id: 1, name: "Rahul" },
-  { id: 2, name: "Amit" }
-];
 
-app.get('/', (req, res) => {
-  res.send("Welcome to Simple Backend API");
-});
 
-app.get('/users', (req, res) => {
-  res.json(users);
-});
 
-app.post('/users', (req, res) => {
-  const newUser = {
-    id: users.length + 1,
-    name: req.body.name
-  };
 
-  users.push(newUser);
+  const PORT = 3001;
 
-  res.json({
-    message: "User added successfully",
-    user: newUser
-  });
-});
+  app.listen(PORT,()=>{
 
-const PORT = 5000;
+    console.log(`server is runnig on 3001${PORT}`)
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+
+  })
+
